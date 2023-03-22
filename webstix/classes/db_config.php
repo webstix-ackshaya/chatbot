afe841382db849893119b05191251cae17a2165d
webstix-ackshaya <126457230+webstix-ackshaya@users.noreply.github.com>
2023-03-22 10:48:29 +0530

    Add files via upload

---
+++ webstix/classes/db_config.php
@@ -0,0 +1,11 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "neutron";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
